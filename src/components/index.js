5366bc89ecaf14df952a019fbae2414b6e03944e
ALIHANVU <alihan95997@gmail.com>
2025-11-27 16:32:44 +0300

    Delete src directory

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-export * from './UI';
-export { Modal, ConfirmModal } from './Modal';
-export { LoginPage } from './Login';
-export { Sidebar } from './Sidebar';
-export { MobileHeader, TabBar, MobilePageHeader } from './MobileNav';
-export * from './Icons';
-export * from './Gestures';
-export * from './PWA';
