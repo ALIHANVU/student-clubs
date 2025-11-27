3556886081a0441bca02e78ff0ef4c7e464cde59
ALIHANVU <alihan95997@gmail.com>
2025-11-27 16:25:45 +0300

    Delete src directory

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export * from './UI';
-export { Modal, ConfirmModal } from './Modal';
-export { LoginPage } from './Login';
-export { Sidebar } from './Sidebar';
-export { MobileHeader, TabBar } from './MobileNav';
--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-export { AdminDashboard } from './AdminDashboard';
-export { StudentDashboard } from './StudentDashboard';
-export { ClubsPage } from './ClubsPage';
-export { EventsPage } from './EventsPage';
-export { ProfilePage } from './ProfilePage';
-export { SchedulePage } from './SchedulePage';
-export { FacultiesPage } from './FacultiesPage';
-export { UsersPage } from './UsersPage';
