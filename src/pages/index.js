36feabe2142dee38358f7687f410f4dec12d9b36
ALIHANVU <alihan95997@gmail.com>
2025-12-06 16:01:26 +0300

    Delete src directory

--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-export { AdminDashboard } from './AdminDashboard';
-export { StudentDashboard } from './StudentDashboard';
-export { ClubsPage } from './ClubsPage';
-export { EventsPage } from './EventsPage';
-export { SchedulePage } from './SchedulePage';
-export { FacultiesPage } from './FacultiesPage';
-export { UsersPage } from './UsersPage';
-export { ProfilePage } from './ProfilePage';
