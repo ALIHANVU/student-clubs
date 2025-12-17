98784f750795acebcefc0cb884f27fe286b7fba7
ALIHANVU <alihan95997@gmail.com>
2025-12-17 10:58:48 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-
 export { AdminDashboard, StudentDashboard } from './Dashboard';
 export { default as ClubsPage } from './ClubsPage';
 export { default as EventsPage } from './EventsPage';
 export { default as SchedulePage } from './SchedulePage';
+export { default as FacultiesPage } from './FacultiesPage';
 export { default as UsersPage } from './UsersPage';
 export { default as ProfilePage } from './ProfilePage';
